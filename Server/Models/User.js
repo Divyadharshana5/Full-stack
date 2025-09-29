212580b9bec4da8d8b2e8684494680a6949a76c0
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-09-29 11:49:54 +0530

    Fix field naming conventions in User schema

--- Server/Models/User.js
+++ Server/Models/User.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    dob: { type: String, required: true },
+    first_Name: { type: String, required: true },
+    last_Name: { type: String, required: true },
+    date_of_birth: { type: String, required: true },
     email: { type: String, required: true },
     gender: { type: String, required: true },
     country: { type: String, required: true },
